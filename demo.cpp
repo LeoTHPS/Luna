3a3898af2e849d13fb78c32aa74690082a5220e0
= <=>
2026-04-24 23:10:18 -0700

    refactor

--- demo.cpp
+++ demo.cpp
@@ -1,6 +1,7 @@
 #include "Luna.hpp"
 
 #include <ctime>
+#include <chrono>
 #include <iostream>
 
 uint32_t time_now()
@@ -11,6 +12,7 @@ uint32_t time_elapsed(uint32_t time)
 {
 	return std::time(nullptr) - time;
 }
+auto     time_unix_timestamp = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::steady_clock::now().time_since_epoch()).count();
 
 int main(int argc, char* argv[])
 {
@@ -20,22 +22,25 @@ int main(int argc, char* argv[])
 
 	if (auto time = luna.CreateTable())
 	{
-		time.SetField("now",     &time_now);
-		time.SetField("elapsed", &time_elapsed);
+		time.SetField("now",            &time_now);
+		time.SetField("elapsed",        &time_elapsed);
+		time.SetField("unix_timestamp", time_unix_timestamp);
 
 		luna.SetGlobal("time", time);
 	}
 
 	if (auto time = luna.CreateLibrary())
 	{
-		time.SetField("now",     &time_now);
-		time.SetField("elapsed", &time_elapsed);
+		time.SetField("now",            &time_now);
+		time.SetField("elapsed",        &time_elapsed);
+		time.SetField("unix_timestamp", time_unix_timestamp);
 
 		luna.LoadLibrary(time, "libtime", true);
 	}
 
-	luna.SetGlobal("time_now",     &time_now);
-	luna.SetGlobal("time_elapsed", &time_elapsed);
+	luna.SetGlobal("time_now",       &time_now);
+	luna.SetGlobal("time_elapsed",   &time_elapsed);
+	luna.SetGlobal("unix_timestamp", time_unix_timestamp);
 
 	try
 	{
--- demo.lua
+++ demo.lua
@@ -1,10 +1,10 @@
-local function do_the_thing(label, now, elapsed)
+local function do_the_thing(label, now, elapsed, unix_timestamp)
 	local time_now     = now();
 	local time_elapsed = elapsed(time_now);
 
-	print(label, time_now, time_elapsed);
+	print(label, unix_timestamp, time_now, time_elapsed);
 end
 
-do_the_thing('global',  time_now, time_elapsed);
-do_the_thing('table',   time.now, time.elapsed);
-do_the_thing('library', libtime.now, libtime.elapsed);
+do_the_thing('global',  time_now, time_elapsed, unix_timestamp);
+do_the_thing('table',   time.now, time.elapsed, time.unix_timestamp);
+do_the_thing('library', libtime.now, libtime.elapsed, libtime.unix_timestamp);
