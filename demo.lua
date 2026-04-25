2af00472dc74d1a42e045a0ecf0312cd2665f1e4
= <=>
2026-04-24 23:18:55 -0700

    refactor

--- demo.lua
+++ demo.lua
@@ -5,6 +5,6 @@ local function do_the_thing(label, now, elapsed, unix_timestamp)
 	print(label, unix_timestamp, time_now, time_elapsed);
 end
 
-do_the_thing('global',  time_now, time_elapsed, unix_timestamp);
-do_the_thing('table',   time.now, time.elapsed, time.unix_timestamp);
+do_the_thing('global',  time_now,    time_elapsed,    unix_timestamp);
+do_the_thing('table',   time.now,    time.elapsed,    time.unix_timestamp);
 do_the_thing('library', libtime.now, libtime.elapsed, libtime.unix_timestamp);
